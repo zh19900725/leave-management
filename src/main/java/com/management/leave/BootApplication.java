b027c711016600b05e17e2e3a894eb20e309473b
aarronzhang <aarronzhang@tencent.com>
2024-03-07 16:54:31 +0800

    基础框架整合、接口梳理

--- src/main/java/com/management/leave/BootApplication.java
+++ src/main/java/com/management/leave/BootApplication.java
@@ -4,8 +4,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ServletComponentScan
 @Aspect
